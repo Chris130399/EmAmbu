3466db6b31493bd65569e053504d1ed78e2152d4
Chris <chrispius99@gmail.com>
2020-02-06 17:04:55 +0530

    Maps Module

---
+++ app/src/main/java/com/example/em_ambu/PayPalConfig.java
@@ -0,0 +1,6 @@
+package com.example.em_ambu;
+
+public class PayPalConfig {
+
+    public static final String PAYAPAL_CLIENT_ID = "AXugiNgwO7IIP89tdv45HbcqHaiglORcLAXSUU0o3-Xtdjcd8QCo6W3q0TveZ8bOgv_cLEB4GTs5Dsi2";
+}
